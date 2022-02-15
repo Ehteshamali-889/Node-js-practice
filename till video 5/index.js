1d937cda46053f7a81de05eaf5b117bed3648b7b
Ehtesham Ali <63139751+Ehteshamali-889@users.noreply.github.com>
2022-02-15 22:40:53 +0500

    Add files via upload

---
+++ till video 5/index.js
@@ -0,0 +1,22 @@
+// Video 2
+// console.log("Welcome");
+// Video 5
+const fs=require("fs");
+// creates and add data Welcome to it
+// fs.writeFileSync('read.txt',"Welcome");
+
+// overrides existing data
+// fs.writeFileSync('read.txt',"Welcome Ali");
+
+// append data to file
+// fs.appendFileSync("read.txt","Bye Ali");
+
+// read file
+// data will store binary data
+// const data=fs.readFileSync("read.txt");
+// console.log(data);
+// org_data=data.toString();
+// console.log(org_data);
+
+// rename file
+// fs.renameSync("read.txt","readwrite.txt");
