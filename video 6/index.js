bf93f6d464e895118377be10ad1f32335a9cf3d5
Ehtesham Ali <63139751+Ehteshamali-889@users.noreply.github.com>
2022-02-16 21:28:09 +0500

    Add files via upload

---
+++ video 6/index.js
@@ -0,0 +1,34 @@
+// Video 2
+// console.log("Welcome");
+// Video 5
+const fs=require("fs");
+// creates and add data Welcome to it
+// fs.writeFileSync('read.txt',"Welcome");
+
+// overrides existing data
+// fs.writeFileSync('read.txt',"Welcome Ali");
+
+// append data to file
+// fs.appendFileSync("read.txt","Bye Ali");
+
+// read file
+// data will store binary data
+// const data=fs.readFileSync("read.txt");
+// console.log(data);
+// org_data=data.toString();
+// console.log(org_data);
+
+// rename file
+// fs.renameSync("read.txt","readwrite.txt");
+
+// challenge
+
+const fs=require("fs");
+// fs.mkdirSync('thapa');
+// fs.writeFileSync('thapa/bio.txt','Hello java');
+// fs.appendFileSync('thapa/bio.txt','By java');
+// const data=fs.readFileSync('thapa/bio.txt');
+// console.log(data);
+// fs.renameSync("thapa/bio.txt","javabio.txt");
+// fs.unlinkSync('thapa/bio.txt');
+// fs.rmdirSync("thapa");
