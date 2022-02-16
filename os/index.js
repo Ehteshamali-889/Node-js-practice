1f2c5e49751df9cdfc906b07c79b800c7999a791
Ehtesham Ali <63139751+Ehteshamali-889@users.noreply.github.com>
2022-02-16 21:49:04 +0500

    Add files via upload

---
+++ os/index.js
@@ -0,0 +1,14 @@
+const os=require("os");
+// console.log(os.arch());
+console.log(os.hostname());
+console.log(os.platform());
+console.log(os.tmpdir());
+console.log(os.type());
+
+// const freemem=os.freemem();
+// console.log(freemem);
+// console.log(`${freemem /1024 / 1024 / 1024}`);
+
+// const totalmem=os.totalmem();
+// console.log(freemem);
+// console.log(`${totalmem /1024 / 1024 / 1024}`);
